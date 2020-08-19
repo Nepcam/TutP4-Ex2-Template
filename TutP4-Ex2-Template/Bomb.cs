50dc891f7b2253dc1a16262b3430cfbecd49a723
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-08-19 13:59:23 +1200

    Bomb class - set up fields and constructor

--- TutP4-Ex2-Template/Bomb.cs
+++ TutP4-Ex2-Template/Bomb.cs
@@ -8,6 +8,18 @@ namespace TutP4_Ex2_Template
 {
     class Bomb
     {
+        private int _x;
+        private int _y;
 
+        /// <summary>
+        /// Initialise the object to the value values passed in
+        /// </summary>
+        /// <param name="x">The x position of the centre of the bomb</param>
+        /// <param name="y">The y position of the centre of the bomb</param>
+        public Bomb(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
     }
 }
